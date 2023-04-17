5f2d7a89c72e89a7351378236453819d8cc1960d
CherylHemphill <126039394+CherylHemphill@users.noreply.github.com>
2023-04-17 15:43:03 -0500

    dateset

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,6 @@ let currentDate = moment().format('llll');
 $("#currentDay").html(currentDate);
 
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $("document").ready(function () {  //defers JS loading until page loaded
 
   //Save button event listener:
@@ -17,18 +13,27 @@ $("document").ready(function () {  //defers JS loading until page loaded
   });
     
     // Apply Past, Present, or Future class
-    let currentHour = moment().hours();
+    let currentHour = moment().format('H');  //get current hour
+    console.log(currentHour);
 
+      $('.time-block').each(function () {   //uses current time block class to set past, present, future
+        let timeBlock = $(this).attr("id").split("-")[1];
 
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
+        if (timeBlock == currentHour) {  //current hour
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+        }
+        else if (timeBlock < currentHour) {    //past hour(s)
+          $(this).removeClass("future");
+          $(this).removeClass("present");
+        }
+        else {                              //future hours
+          $(this).removeClass("present");
+          $(this).removeClass("past");
+        }
+
+      });
+
     
     //Show saved local storage by each hour
     $('#hour-8 .description').val(localStorage.getItem('hour-8'));
@@ -43,7 +48,5 @@ $("document").ready(function () {  //defers JS loading until page loaded
     $('#hour-17 .description').val(localStorage.getItem('hour-17'));
     $('#hour-18 .description').val(localStorage.getItem('hour-18'));
 
-    // TODO: Add code to display the current date in the header of the page.
-  
   
    });  //Ends defer function
